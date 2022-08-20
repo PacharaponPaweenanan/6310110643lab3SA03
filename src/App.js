792d41db71794c4ab755777511c2e89de73e6ec3
Pacaarapon643 <peemmy2012@gmail.com>
2022-08-20 11:26:32 +0700

    word

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import './App.css';
 import WordCard from './WordCard';
 
 const word = "Hello";
+const word2 = "LOVE";
+const word3 = "BOOK";
 function App() {
 return (
   <div>
      <WordCard value={word}/>
+     <WordCard value={word2}/>
+     <WordCard value={word3}/>
+
   </div>
   );
 }
