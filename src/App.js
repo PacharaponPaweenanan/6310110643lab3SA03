864066f97ab2f8a56f1d59ff243f6e7c0556565f
Pacaarapon643 <peemmy2012@gmail.com>
2022-08-20 11:48:20 +0700

    h1

--- src/App.js
+++ src/App.js
@@ -5,9 +5,13 @@ import WordCard from './WordCard';
 const word = "Hello";
 const word2 = "LOVE";
 const word3 = "BOOK";
+const textInput = <input type="text" />
 function App() {
 return (
   <div>
+    <h1>GameCard</h1>
+    <p>ชื่อผู้เล่น</p>
+    <p>{textInput}</p>
      <WordCard value={word}/>
      <WordCard value={word2}/>
      <WordCard value={word3}/>
