5248f58130e53d6dcc65dd570321e9715a25c795
Pacaarapon643 <peemmy2012@gmail.com>
2022-08-20 12:14:19 +0700

    color

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import WordCard from './WordCard';
 const word = "Hello";
 const word2 = "LOVE";
 const word3 = "BOOK";
+const word4 = "kill";
+const word5 = "school";
 const textInput = <input type="text" />
 function App() {
 return (
-  <div>
-    <h1>GameCard</h1>
-    <p>ชื่อผู้เล่น</p>
+  <div className='backgroung'>
+    <h1 className='head'>GameCard</h1>
+    <p className='play'>NamePalyer</p>
     <p>{textInput}</p>
      <WordCard value={word}/>
      <WordCard value={word2}/>
      <WordCard value={word3}/>
-
+     <WordCard value={word4}/>
+     <WordCard value={word5}/>
   </div>
   );
 }
